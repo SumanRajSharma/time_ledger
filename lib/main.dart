734f9ab2802695af275b9ca73502f4074cf97f9f
Suman Raj Sharma <sumanrajsharma@sumans-mbp.lan>
2024-05-30 19:15:30 +1000

    Enhance main.dart with theme and loader overlay integration
    - Added new imports for flutter_spinkit, loader_overlay, app_text_themes, and theme_bloc
    - Implemented theme management using ThemeBloc and ThemeState
    - Configured light and dark color schemes based on theme state
    - Integrated GlobalLoaderOverlay with custom loading spinner and blur effect
    - Updated MaterialApp configuration to utilize new theme settings and loader overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+import 'package:time_ledger/config/theme/app_text_themes.dart';
 import 'package:time_ledger/core/navigation/app_router_delegate.dart';
 import 'package:time_ledger/core/navigation/app_state.dart';
 import 'package:time_ledger/core/navigation/page_information_parser.dart';
@@ -8,10 +12,11 @@ import 'package:time_ledger/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:time_ledger/features/auth/presentation/bloc/auth_event.dart';
 import 'package:time_ledger/features/auth/presentation/bloc/splash_navigation_bloc.dart';
 import 'package:time_ledger/features/auth/presentation/bloc/splash_navigation_event.dart';
+import 'package:time_ledger/features/user/presentation/bloc/theme_bloc.dart';
+import 'package:time_ledger/features/user/presentation/bloc/theme_state.dart';
 import 'injection_container.dart' as di;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:time_ledger/config/theme/app_themes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
@@ -27,11 +32,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Dependency Injection
     final appState = di.sl<AppState>();
     final manageAuth = di.sl<ManageAuthentication>();
     final routerDelegate = AppRouterDelegate(manageAuth, appState);
     final routeParser = PageInformationParser();
-
+    TextTheme textTheme = createTextTheme(context, 'Urbanist', 'Urbanist');
     // Check the session status on app start
     manageAuth.checkSession();
 
@@ -45,13 +51,80 @@ class MyApp extends StatelessWidget {
         BlocProvider<AuthBloc>(
           create: (context) => di.sl<AuthBloc>()..add(const CheckSession()),
         ),
+        BlocProvider<ThemeBloc>(
+          create: (context) => di.sl<ThemeBloc>(),
+        ),
       ],
-      child: MaterialApp.router(
-        title: 'Time Ledger',
-        debugShowCheckedModeBanner: false,
-        theme: theme(),
-        routerDelegate: routerDelegate,
-        routeInformationParser: routeParser,
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, themeState) {
+          final lightColorScheme = ColorScheme.fromSeed(
+            seedColor: themeState.seedColor,
+          );
+
+          final darkColorScheme = ColorScheme.fromSeed(
+            seedColor: themeState.seedColor,
+            brightness: Brightness.dark,
+          );
+
+          return GlobalLoaderOverlay(
+            useDefaultLoading: false,
+            overlayWidgetBuilder: (dynamic progress) {
+              return BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 4.5,
+                  sigmaY: 4.5,
+                ),
+                child: Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .primaryContainer
+                          .withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SpinKitPouringHourGlass(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        const SizedBox(height: 14),
+                        if (progress != null)
+                          Text(
+                            progress,
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+            child: MaterialApp.router(
+              title: 'Time Ledger',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                  useMaterial3: true,
+                  colorScheme: lightColorScheme,
+                  scaffoldBackgroundColor: lightColorScheme.surface,
+                  textTheme: textTheme),
+              darkTheme: ThemeData(
+                  useMaterial3: true,
+                  colorScheme: darkColorScheme,
+                  scaffoldBackgroundColor: darkColorScheme.surface,
+                  textTheme: textTheme),
+              themeMode: themeState.themeMode,
+              routerDelegate: routerDelegate,
+              routeInformationParser: routeParser,
+            ),
+          );
+        },
       ),
     );
   }
